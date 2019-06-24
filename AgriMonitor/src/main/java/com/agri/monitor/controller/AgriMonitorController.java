b7687d559f87a3446dff6163db9fcd72924139e1
cuckoolll <349670059@qq.com>
2019-06-24 23:15:25 +0800

    前台页面
    前台页面

--- AgriMonitor/src/main/java/com/agri/monitor/controller/AgriMonitorController.java
+++ AgriMonitor/src/main/java/com/agri/monitor/controller/AgriMonitorController.java
@@ -1,14 +1,42 @@
 package com.agri.monitor.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/Agri")
+@Controller
 public class AgriMonitorController {
-	@RequestMapping("/hello")
-	public String hello() {
-		return "hello!";
+	@RequestMapping("/index.html")
+	public String index() {
+		return "index";
 	}
-
+	
+	@RequestMapping("/components.html")
+	public String components() {
+		return "components";
+	}
+	
+	@RequestMapping("/forms.html")
+	public String forms() {
+		return "forms";
+	}
+	
+	@RequestMapping("/icons.html")
+	public String icons() {
+		return "icons";
+	}
+	
+	@RequestMapping("/notifications.html")
+	public String notifications() {
+		return "notifications";
+	}
+	
+	@RequestMapping("/tables.html")
+	public String tables() {
+		return "tables";
+	}
+	
+	@RequestMapping("/typography.html")
+	public String typography() {
+		return "typography";
+	}
 }
