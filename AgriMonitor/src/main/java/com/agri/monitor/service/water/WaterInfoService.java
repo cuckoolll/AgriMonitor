b0d70204b18f442bdba6cc2252a8e144a418c100
duyu <duyu@WH-DUYU.ygsoft.com>
2019-06-29 18:23:38 +0800

    水质监测查询

---
+++ AgriMonitor/src/main/java/com/agri/monitor/service/water/WaterInfoService.java
@@ -0,0 +1,29 @@
+package com.agri.monitor.service.water;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.agri.monitor.entity.WaterInfo;
+import com.agri.monitor.mapper.water.WaterInfoMapper;
+
+@Service
+@Transactional
+public class WaterInfoService {
+	@Autowired
+	private WaterInfoMapper waterInfoMapper;
+	
+	@Value("${spring.profiles.active}")  
+	private String active;  
+	
+	public List<WaterInfo> findAll() {
+		return waterInfoMapper.findAll();
+	}
+	
+	public List<WaterInfo> queryInfoByCountryAndTime(final String county, final String time) {
+		return waterInfoMapper.queryInfoByCountryAndTime(county, time);
+	}
+}
