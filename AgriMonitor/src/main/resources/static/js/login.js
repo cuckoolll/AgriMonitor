e88a9baa3aa62b1188572cd88ff2cfc73b402636
cuckoo <349670059@qq.com>
2019-08-09 23:19:00 +0800

    登录页缺陷修复

--- AgriMonitor/src/main/resources/static/js/login.js
+++ AgriMonitor/src/main/resources/static/js/login.js
@@ -1,4 +1,18 @@
-layui.use(['form', 'layer'], function(form){
+layui.use(['form', 'layer'], function(form, layer){
+	
+	//自定义验证规则
+//	form.verify({
+//		vercodes: function(value){
+//			//获取验证码
+//			var zylVerCode = $(".zylVerCode").html();
+//			if (value == zylVerCode) {
+//				verCodeTrue = true; 
+//			} else {
+//				layer.msg("验证码错误（区分大小写）");
+//			}
+//		}
+//	});
+	
 	//监听提交
 	form.on('submit(login)', function(data){
 		login()
@@ -11,16 +25,26 @@ layui.use(['form', 'layer'], function(form){
 	function login() {
 		var user_id=$("#user_id").val();
 		var user_password=$("#user_password").val();
-		if(user_id && user_password){
+		if (user_id && user_password) {
+			var zylVerCode = $(".zylVerCode").html();
+			var vercode = $("#vercode").val();
+			if (!vercode) {
+				layer.msg("请输入验证码");
+				return;
+			}
+			if (vercode != zylVerCode) {
+				layer.msg("验证码错误（区分大小写）");
+				return;
+			}
 			$.post("/doLogin", {userid: user_id, pw: user_password},function(data){
 		          if(data && data.user_id){
 		        	  window.location.href="/workbench"; 
 		          }else{
-		        	  layer.msg("用户名或密码错误！");
+		        	  layer.msg("用户名或密码错误");
 		          }
 	        });
 		}else{
-			layer.msg("请输入用户名和密码！");
+			layer.msg("请输入用户名和密码");
 		}
 	}
 	
