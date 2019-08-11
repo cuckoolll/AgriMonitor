1e5d6c09c1ae6030dfd40d0a9892bcca5dc89981
huguangming <1054756573@qq.com>
2019-08-11 22:05:02 +0800

    代码提交

--- AgriMonitor/target/classes/static/js/dataoverview.js
+++ AgriMonitor/target/classes/static/js/dataoverview.js
@@ -480,6 +480,50 @@ layui.use(['table'], function(table) {
 		});
 	}
 	
+	function monitorinfo(){
+		 $.post("/monitorManage/findmonitorinfo", {},function(res){
+		        if(res && res.length>0){
+		        	$("#monitorSpan",parent.document).show();
+		        	$(".box",parent.document).show();
+		        	var str="";
+		        	$.each(res,function(index,item){
+		        		str+="<p class='item'>"+item+"</p>";
+		        	});
+		        	$("#monitorinfo",parent.document).html(str);
+		        	
+		        	if(res.length>1){
+		        		var wrapper  = parent.document.getElementsByClassName('wrapper')[0];
+		        		  var offset = 50
+		        		  var timer;
+		        		  setInterval(function () {
+		        		    if(timer) {
+		        		      clearInterval(timer);
+		        		    }
+		        		    var step = 1;
+		        		  
+		        		    timer = setInterval(function () {
+		        		      wrapper.style.transform = 'translateY(-' + (offset + step) + 'px)';
+		        		      if(step == 50) {
+		        		        clearInterval(timer);
+		        		      }
+		        		      step++;
+		        		    }, 10);
+		        		    
+		        		    offset += 50;
+		        		    var num = Math.floor(wrapper.offsetHeight / 50)
+		        		    if (! (offset%((num - 1)*50))) {
+		        		      offset = 0;
+		        		    }
+		        		  }, 8000);
+		        	}
+		        } else {
+		        	$("#monitorSpan",parent.document).hide();
+		        	$(".box",parent.document).hide();
+		        }
+		    });
+	}
+	
+	monitorinfo();
 	initchart();
 	farmdata();
 	animalsBreedData();
