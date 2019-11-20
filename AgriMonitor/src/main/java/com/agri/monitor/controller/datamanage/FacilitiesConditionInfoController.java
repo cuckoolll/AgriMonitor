b96e62eb671db6c0f206e41a57e61474902f662b
duyu <duyu@WH-DUYU.ygsoft.com>
2019-11-20 19:50:54 +0800

    农业装备信息

--- AgriMonitor/src/main/java/com/agri/monitor/controller/datamanage/FacilitiesConditionInfoController.java
+++ AgriMonitor/src/main/java/com/agri/monitor/controller/datamanage/FacilitiesConditionInfoController.java
@@ -1,35 +1,39 @@
 package com.agri.monitor.controller.datamanage;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.agri.monitor.annotation.IgnoreSession;
+import com.agri.monitor.entity.FacilityConditionInfo;
 import com.agri.monitor.entity.UserInfo;
 import com.agri.monitor.enums.CacheTypeEnum;
+import com.agri.monitor.service.datamanage.FacilitiesConditionInfoService;
 import com.agri.monitor.utils.CacheUtil;
-import com.agri.monitor.vo.EnvironmentQueryVO;
+import com.agri.monitor.vo.FacilityConditionQueryVO;
 
 @Controller
 @RequestMapping("/facilitiesconditioninfo")
 public class FacilitiesConditionInfoController {
 	
-//	@Autowired
-//	private EnvironmentInfoService environmentInfoService;
+	@Autowired
+	private FacilitiesConditionInfoService facilitiesConditionInfoService;
 	
 	/**
 	 * 草原生态监测页面 .
 	 * @return .
 	 */
 	@RequestMapping("")
-	public String proTeamAndEmployInfo(Model model) {
+	public String facilityConditionInfo(Model model) {
 		model.addAttribute("towns", CacheUtil.getCache(CacheTypeEnum.TOWNS));
 		return "/datamanage/facilitiesconditioninfo/facilitiesconditioninfo";
 	}
@@ -37,15 +41,9 @@ public class FacilitiesConditionInfoController {
 	@RequestMapping(value="/queryInfo", method = RequestMethod.POST)
 	@ResponseBody
 	@IgnoreSession
-	public Map queryInfo(EnvironmentQueryVO queryVo, HttpServletRequest request) {
+	public Map queryInfo(FacilityConditionQueryVO queryVo, HttpServletRequest request) {
 		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
-//		return environmentInfoService.queryInfoForPage(queryVo, user.getUser_id());
-		final Map param = new HashMap();
-		param.put("code", 0);
-		param.put("msg", "成功");
-		param.put("count", 0);
-		param.put("data", new HashMap());
-		return param;
+		return facilitiesConditionInfoService.queryInfoForPage(queryVo, user.getUser_id());
 	}
 	
 //	@ResponseBody
@@ -54,31 +52,31 @@ public class FacilitiesConditionInfoController {
 //		return environmentInfoService.dataImport(file, request);
 //	}
 	
-//	@IgnoreSession
-//	@RequestMapping("/update")
-//	public String add(Model model) {
-//		return "/datamanage/environmentinfo/environmentupdate";
-//	}
+	@IgnoreSession
+	@RequestMapping("/update")
+	public String add(Model model) {
+		return "/datamanage/facilitiesconditioninfo/facilitiesconditionupdate";
+	}
 
-//	@ResponseBody
-//	@RequestMapping(value="/save",method=RequestMethod.POST)
-//	public Map doUpdate(EnvironmentInfo environmentinfo, HttpServletRequest request) {
-//		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
-//		return environmentInfoService.saveOrUpdate(environmentinfo, user.getUser_id());
-//	}
+	@ResponseBody
+	@RequestMapping(value="/save",method=RequestMethod.POST)
+	public Map doUpdate(FacilityConditionInfo facilityconditioninfo, HttpServletRequest request) {
+		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
+		return facilitiesConditionInfoService.saveOrUpdate(facilityconditioninfo, user.getUser_id());
+	}
 	
-//	@ResponseBody
-//	@RequestMapping(value="/delInfoByGid",method=RequestMethod.POST)
-//	public Map delInfoByGid(@RequestBody ArrayList<Integer> gids, HttpServletRequest request) {
-//		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
-//		return environmentInfoService.delInfoByGid(gids, user.getUser_id());
-//	}
+	@ResponseBody
+	@RequestMapping(value="/delInfoByGid",method=RequestMethod.POST)
+	public Map delInfoByGid(@RequestBody ArrayList<Integer> gids, HttpServletRequest request) {
+		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
+		return facilitiesConditionInfoService.delInfoByGid(gids, user.getUser_id());
+	}
 	
-//	@ResponseBody
-//	@RequestMapping(value="/findById",method=RequestMethod.POST)
-//	public EnvironmentInfo findById(Integer gid, HttpServletRequest request) {
-//		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
-//		return environmentInfoService.findById(gid, user.getUser_id());
-//	}
+	@ResponseBody
+	@RequestMapping(value="/findById",method=RequestMethod.POST)
+	public FacilityConditionInfo findById(Integer gid, HttpServletRequest request) {
+		UserInfo user = (UserInfo) request.getSession().getAttribute("userinfo");
+		return facilitiesConditionInfoService.findById(gid, user.getUser_id());
+	}
 	
 }
---
+++ AgriMonitor/src/main/java/com/agri/monitor/entity/FacilityConditionInfo.java
@@ -0,0 +1,181 @@
+package com.agri.monitor.entity;
+
+public class FacilityConditionInfo { 
+	private String gid; 
+	private String date_year; 
+	private String county;
+	private String towns;
+	private String village;
+	private String agri_address;
+	private double large_medium_tractors;
+	private double small_walking_tractors;
+	private double combine_harvester;
+	private double motor_thresher; 
+	private double agricultural_carriage_car;
+	private double rural_hydropower_stations; 
+	private double generating_capacity; 
+	private double electric_energy;
+	private double rural_power_consumption;
+	private double effective_irrigation_area;
+	private double waterlogging_drought_area;
+	private double mechanical_irrigation_area;
+	private double reservoir;
+	private double channel_area;
+	private String remark; 
+	private String create_time; 
+	private String creator; 
+	private String last_time; 
+	private String modifier;
+	public String getGid() {
+		return gid;
+	}
+	public void setGid(String gid) {
+		this.gid = gid;
+	}
+	public String getDate_year() {
+		return date_year;
+	}
+	public void setDate_year(String date_year) {
+		this.date_year = date_year;
+	}
+	public String getCounty() {
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	public String getTowns() {
+		return towns;
+	}
+	public void setTowns(String towns) {
+		this.towns = towns;
+	}
+	public String getVillage() {
+		return village;
+	}
+	public void setVillage(String village) {
+		this.village = village;
+	}
+	public String getAgri_address() {
+		return agri_address;
+	}
+	public void setAgri_address(String agri_address) {
+		this.agri_address = agri_address;
+	}
+	public double getLarge_medium_tractors() {
+		return large_medium_tractors;
+	}
+	public void setLarge_medium_tractors(double large_medium_tractors) {
+		this.large_medium_tractors = large_medium_tractors;
+	}
+	public double getSmall_walking_tractors() {
+		return small_walking_tractors;
+	}
+	public void setSmall_walking_tractors(double small_walking_tractors) {
+		this.small_walking_tractors = small_walking_tractors;
+	}
+	public double getCombine_harvester() {
+		return combine_harvester;
+	}
+	public void setCombine_harvester(double combine_harvester) {
+		this.combine_harvester = combine_harvester;
+	}
+	public double getMotor_thresher() {
+		return motor_thresher;
+	}
+	public void setMotor_thresher(double motor_thresher) {
+		this.motor_thresher = motor_thresher;
+	}
+	public double getAgricultural_carriage_car() {
+		return agricultural_carriage_car;
+	}
+	public void setAgricultural_carriage_car(double agricultural_carriage_car) {
+		this.agricultural_carriage_car = agricultural_carriage_car;
+	}
+	public double getRural_hydropower_stations() {
+		return rural_hydropower_stations;
+	}
+	public void setRural_hydropower_stations(double rural_hydropower_stations) {
+		this.rural_hydropower_stations = rural_hydropower_stations;
+	}
+	public double getGenerating_capacity() {
+		return generating_capacity;
+	}
+	public void setGenerating_capacity(double generating_capacity) {
+		this.generating_capacity = generating_capacity;
+	}
+	public double getElectric_energy() {
+		return electric_energy;
+	}
+	public void setElectric_energy(double electric_energy) {
+		this.electric_energy = electric_energy;
+	}
+	public double getRural_power_consumption() {
+		return rural_power_consumption;
+	}
+	public void setRural_power_consumption(double rural_power_consumption) {
+		this.rural_power_consumption = rural_power_consumption;
+	}
+	public double getEffective_irrigation_area() {
+		return effective_irrigation_area;
+	}
+	public void setEffective_irrigation_area(double effective_irrigation_area) {
+		this.effective_irrigation_area = effective_irrigation_area;
+	}
+	public double getWaterlogging_drought_area() {
+		return waterlogging_drought_area;
+	}
+	public void setWaterlogging_drought_area(double waterlogging_drought_area) {
+		this.waterlogging_drought_area = waterlogging_drought_area;
+	}
+	public double getMechanical_irrigation_area() {
+		return mechanical_irrigation_area;
+	}
+	public void setMechanical_irrigation_area(double mechanical_irrigation_area) {
+		this.mechanical_irrigation_area = mechanical_irrigation_area;
+	}
+	public double getReservoir() {
+		return reservoir;
+	}
+	public void setReservoir(double reservoir) {
+		this.reservoir = reservoir;
+	}
+	public double getChannel_area() {
+		return channel_area;
+	}
+	public void setChannel_area(double channel_area) {
+		this.channel_area = channel_area;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(String create_time) {
+		this.create_time = create_time;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getLast_time() {
+		return last_time;
+	}
+	public void setLast_time(String last_time) {
+		this.last_time = last_time;
+	}
+	public String getModifier() {
+		return modifier;
+	}
+	public void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+	
+	
+}
